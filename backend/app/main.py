81da81989d65f0b9f588c5e3afb5e272186cb3a8
Helmanz <helcze4@gmail.com>
2024-06-21 18:10:03 +0200

    more endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 import os
+from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Session
+from db.database import commit_and_handle_exception, get_session, refresh_and_handle_exception
 from db.db_init import init_db
-from fastapi import FastAPI
+from db.schemas import Card, Task, Workspace
+from fastapi import Depends, FastAPI
 from routers import users, auth
+from routers.auth import Token
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -24,5 +29,47 @@ app.add_middleware(
     allow_headers=CORS_ALLOWED_HEADERS,
     max_age=CORS_MAX_AGE,
 )
+
+db_dependency = Annotated[Session, Depends(get_session)]
+
+@app.post("/createWorkspace", response_model=Workspace)
+async def createWorkspace(token: str, session: db_dependency):
+    user_token = Token.decode_access_token(token)
+    if user_token:
+        workspace = Workspace(
+            name="My Workspace", 
+            owner_id=user_token["user_id"]
+        )
+        session.add(workspace)
+        commit_and_handle_exception(session)
+        refresh_and_handle_exception(session, workspace)
+        return workspace
+    
+@app.post("/createCard/{workspace_id}", response_model=Card)
+async def createCard(workspace_id: int, token: str, session: db_dependency):
+    user_token = Token.decode_access_token(token)
+    if user_token:
+        card = Card(
+            name="My Card", 
+            workspace_id=workspace_id
+        )
+        session.add(card)
+        commit_and_handle_exception(session)
+        refresh_and_handle_exception(session, card)
+        return card
+    
+@app.post("/createTask/{workspace_id}/{card_id}", response_model=Task)
+async def createTask(workspace_id: int, card_id: int, token: str, session: db_dependency):
+    user_token = Token.decode_access_token(token)
+    if user_token:
+        task = Task(
+            name="My Task", 
+            workspace_id=workspace_id, 
+            card_id=card_id
+        )
+        session.add(task)
+        commit_and_handle_exception(session)
+        refresh_and_handle_exception(session, task)
+        return task
 
 init_db()
